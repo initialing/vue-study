dc9978d886f563f366851ae078f8c92c0ed458f7
郑秋桐 <scholes.zheng@xfruit.cn>
2018-12-13 17:06:01 +0800

    完善ai接口

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -73,17 +73,22 @@ export default {
             let reader = new FileReader()
             let imgfile
             reader.readAsDataURL(f)
-            reader.onload = e=>{
+            reader.onload = async e=>{
                 imgfile = e.target.result
                 datas['app_id'] = 2110493895
-                datas['time_stamp'] = f.lastModified
+                let times = new Date().getTime()
+                times = times.toString()
+                times = times.substring(0,10)
+                datas['time_stamp'] = parseInt(times)
                 datas['nonce_str'] = me.randomWord(true,15,32)
-                // datas['image'] = this.imgbase.split('base64,')[1]
+                // datas['image'] = imgfile
                 datas['image'] = imgfile.split('base64,')[1]
+                // datas['image_url'] = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544690571192&di=a20f11325d332586292be443c5e8cd07&imgtype=0&src=http%3A%2F%2Fimg0.ph.126.net%2FRYh0GlXytfdbaPFMjUriyw%3D%3D%2F6598262736773960181.png'
                 let ddd = me.getReqSign(datas,appkey)
                 datas['sign'] = ddd
                 console.log(datas)
                 console.log('ddd===>',ddd)
+                console.log('image',datas.image)
                 me.$http.post("https://api.ai.qq.com/fcgi-bin/image/image_terrorism",datas).then(res=>{
                     console.log('res=====>',res)
                 })
@@ -109,11 +114,15 @@ export default {
                 keys.push(i)
             }
             keys = keys.sort()
+            console.log('keys===>',keys)
             let str = ''
+            console.log('encode test====>',this.urlencode('疼 训'))
             for(let j=0;j<keys.length;j++){
-                str=str + keys[j] + '=' + encodeURI(datas[keys[j]]) + '&'
+                str=str + keys[j] + '=' + this.urlencode(datas[keys[j]]) + '&'
+                // str = str + keys[j] + '=' + datas[keys[j]] + '&'
             }
             str = str + 'app_key=' + appkey
+            // console.log('str====>',str)
             let mstr = md5(str).toUpperCase()
             return mstr
         },
@@ -131,6 +140,66 @@ export default {
         async base(f){
             let ff = await this.getbase(f)
             this.imgbase = ff
+        },
+        urlencode(clearString) 
+        {
+            var output = '';
+            var x = 0;
+            
+            clearString = utf16to8(clearString.toString());
+            var regex = /(^[a-zA-Z0-9-_.]*)/;
+
+            while (x < clearString.length) 
+            {
+                var match = regex.exec(clearString.substr(x));
+                if (match != null && match.length > 1 && match[1] != '') 
+                {
+                    output += match[1];
+                    x += match[1].length;
+                } 
+                else 
+                {
+                    if (clearString[x] == ' ')
+                        output += '+';
+                    else 
+                    {
+                        var charCode = clearString.charCodeAt(x);
+                        var hexVal = charCode.toString(16);
+                        output += '%' + ( hexVal.length < 2 ? '0' : '' ) + hexVal.toUpperCase();
+                    }
+                    x++;
+                }
+            }
+
+            function utf16to8(str) 
+            {
+                var out, i, len, c;
+
+                out = "";
+                len = str.length;
+                for(i = 0; i < len; i++) 
+                {
+                    c = str.charCodeAt(i);
+                    if ((c >= 0x0001) && (c <= 0x007F)) 
+                    {
+                        out += str.charAt(i);
+                    } 
+                    else if (c > 0x07FF) 
+                    {
+                        out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+                        out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
+                        out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+                    } 
+                    else 
+                    {
+                        out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
+                        out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+                    }
+                }
+                return out;
+            }
+
+            return output;
         }
     }
 }
